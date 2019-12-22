4752f4d11da0bdacac6d5dc0fccd6f3716a01e0a
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-22 19:16:45 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,7 @@
             }
 
             .content {
+            
             }
 
             .title {
@@ -69,35 +70,30 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Thanks
-                <ol></ol>
-                    For
-                <ol></ol>
-                    Coming
-                </div>
-                <div class="links">
-                    <a href="https://laravel.com/docs">Works</a>
-                    <a href="https://github.com/ryoryo12">GitHub</a>
-                    <a href="https://laravel-news.com">Contact</a>
-                </div>
+        <div class="content">
+            <div class="title m-b-md">
+                Thanks
+            <ol></ol>
+                For
+            <ol></ol>
+                Coming
             </div>
+            <div class ="left_content">
+            <div class="links">
+                <a href="https://laravel.com/docs">Works</a>
+                <a href="https://github.com/ryoryo12">GitHub</a>
+                <a href="https://laravel-news.com">Contact</a>
+            </div>
+        
+        <div class ="right_content">
+            I'm Ryota
+        <ol></ol>
+            I will work as a system engineer
+        <ol></ol>
+            based in Tokyo
+
+        </div>
+            
         </div>
     </body>
 </html>
