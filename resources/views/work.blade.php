150dcf41f6cadc5a65a8e4d0f9eb573c87d26c68
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-23 00:14:51 +0900

    Update work.blade.php

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -1,1 +1,12 @@
-Hello World
+@extends('common.layout')
+@section('addCSS')
+<link href="{{{asset('/assets/css/〇〇.css')}}}" rel="stylesheet">
+@stop
+@include('common.header')
+@section('content')
+    <h1>HelloWorld</h1>
+@stop
+@section('addJS')
+<script type="text/javascript" src="{{{asset('/assets/js/〇〇.js')}}}"></script>
+@stop
+@include('common.footer')
