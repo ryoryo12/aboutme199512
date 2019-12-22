1d592273327797212fdb0d2811bcf93203c0c7f8
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-23 00:12:11 +0900

    Create header.blade.php

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,18 @@
+@section('header')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="utf-8">
+<title>Laravelでサイト作ってるよ</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+@yield('addCss')
+<!--[if lt IE 9]>
+<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js">
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js">
+<![endif]-->
+</head>
+<body>
+@stop
