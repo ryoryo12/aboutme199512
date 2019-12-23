e5fe1e6a251bec597cb435d92dbc10d1f398f361
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-23 19:51:36 +0900

    Update contact_form.blade.php

--- resources/views/contact_form.blade.php
+++ resources/views/contact_form.blade.php
@@ -1,35 +1,42 @@
 <?php
 session_start();
- 
+
 header("Content-type: text/html; charset=utf-8");
- 
+
 //クリックジャッキング対策
 header('X-FRAME-OPTIONS: SAMEORIGIN');
- 
+
 ?>
- 
+
 <!DOCTYPE html>
 <html>
-<head>
-<title>メールフォーム</title>
-<meta charset="utf-8">
-</head>
-<body>
- 
-<p> 全ての項目が必須です。</p>
- 
-<form action="/contact_check" method="post">
+    <head>
 
-@csrf
- 
-<p>名前：<input type="text" name="name"></p>
-<p>メールアドレス：<input type="text" name="mail" size="50"></p>
-<p><textarea name="comment" rows="4" cols="40">コメントをどうぞ</textarea></p>
- 
-<input type="hidden" name="token" value="<?=sha1(session_id())?>">
-<input type="submit" value="確認する">
- 
-</form>
- 
-</body>
+        <meta charset="utf-8">
+
+        <title>メールフォーム</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <style>
+        </style>
+      </head>
+      <body>
+
+          <p> 全ての項目が必須です。</p>
+          
+          <form action="/contact_check" method="post">
+
+          @csrf
+          
+          <p>名前：<input type="text" name="name"></p>
+          <p>メールアドレス：<input type="text" name="mail" size="50"></p>
+          <p><textarea name="comment" rows="4" cols="40">コメントをどうぞ</textarea></p>
+          
+          <input type="hidden" name="token" value="<?=sha1(session_id())?>">
+          <input type="submit" value="確認する">
+          
+          </form>
+
+    </body>
 </html>
