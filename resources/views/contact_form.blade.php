dcc7b052ae15584240ed5f7d85439b9489726fe7
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-25 22:23:05 +0900

    Update contact_form.blade.php

--- resources/views/contact_form.blade.php
+++ resources/views/contact_form.blade.php
@@ -30,8 +30,8 @@ header('X-FRAME-OPTIONS: SAMEORIGIN');
           }
 
           .content{
-            height: 100vh;
-            width: 100vh;
+            height: 100%;
+            width: 100%;
             margin: 0 auto;
           }
 
@@ -43,26 +43,61 @@ header('X-FRAME-OPTIONS: SAMEORIGIN');
             text-align: center;
           }
 
+          .left_content{
+                width: 14%;
+                top: 0px;
+                position: fixed;
+                border-right: solid 1px #FFA500;
+            }
+
+            .right_content{
+                width: 85%;
+                position: absolute;
+                right: 0px;
+                border-left: solid 1px #FF8C00;
+                height: 100%;
+            }
+
+          a {
+              display: block;
+              margin: 150px; 
+              transform: rotate(270deg);
+              color: #777777;
+              right: 0;
+              font-size: 22px;
+              font-weight: 600;
+              letter-spacing: .1rem;
+              text-decoration: none;
+              text-transform: uppercase;
+          }
+
           
         </style>
       </head>
       <body>
         <div class="content">
-            <h1> お問い合わせフォーム</h1>
-            
-            <form action="/contact_check" method="post">
-
-            @csrf
-            
-                <p>名前：<input type="text" name="name"></p>
-                <p>メールアドレス：<input type="text" name="mail" size="50"></p>
-                <p><textarea name="comment" rows="4" cols="40">コメントをどうぞ</textarea></p>
+            <div class ="left_content">
+                <ul class="links">
+                    <a href="/work">Works</a>
+                    <a href="https://github.com/ryoryo12">GitHub</a>
+                    <a href="https://www.facebook.com/ryota.hirata.986">Facebook</a>
+                    <a href="/contact_form">Contact</a>
+                </ul>
+            </div>
+            <div class="right_content">
+                <h1> お問い合わせフォーム</h1>
+                <form action="/contact_check" method="post">
+                @csrf
                 
-                <input type="hidden" name="token" value="<?=sha1(session_id())?>">
-                <input type="submit" value="確認する">
-            
-            </form>
-          </div>
+                    <p>名前：<input type="text" name="name"></p>
+                    <p>メールアドレス：<input type="text" name="mail" size="50"></p>
+                    <p><textarea name="comment" rows="4" cols="40">コメントをどうぞ</textarea></p>
+                    
+                    <input type="hidden" name="token" value="<?=sha1(session_id())?>">
+                    <input type="submit" value="確認する">
+                </form>
+            </div>
+        </div>
 
     </body>
 </html>
