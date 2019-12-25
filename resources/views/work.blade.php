6fd7aaee92072ac7ec6e9813c9d58906aa79a2ae
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-25 14:26:12 +0900

    Update work.blade.php

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -94,7 +94,7 @@
                 <div class="card mb-3">
                     <div class="row no-gutters">
                         <div class="col-md-4">
-                            <img src="..." class="card-img" alt="...">
+                            <img src="https://i.gyazo.com/cc3d8974487acb293c451b38a4ce3b94.jpg" class="card-img" alt="...">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
@@ -105,21 +105,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="card mb-3">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
-                            <img src="..." class="card-img" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    
 
             </div>
         </div>
