0adfdce10aa4632e320376c612d3af815b968179
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-29 16:23:53 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
         <title>About_Me</title>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/105/three.min.js"></script>
             <script>
 
             $(function() {
