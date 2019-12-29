8dd56c57d4b03ee85dc7c0b5ef3452cc7e7176eb
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-29 16:23:19 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,14 +70,14 @@
                         }
                     }
 
-            // $(function() {
-            //     setTimeout(function(){
-            //         $('.start p').fadeIn(1600);
-            //     },500); //0.5秒後にロゴをフェードイン!
-            //     setTimeout(function(){
-            //         $('.start').fadeOut(500);
-            //     },2000); //2秒後にロゴ含め真っ白背景をフェードアウト！
-            // });
+            $(function() {
+                setTimeout(function(){
+                    $('.start p').fadeIn(1600);
+                },500); //0.5秒後にロゴをフェードイン!
+                setTimeout(function(){
+                    $('.start').fadeOut(500);
+                },2000); //2秒後にロゴ含め真っ白背景をフェードアウト！
+            });
 
             </script>
         </script>
