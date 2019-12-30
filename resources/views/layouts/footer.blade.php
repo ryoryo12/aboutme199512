fba8722b6f4f1aba6e2e7e41c8e767bfe468ba6c
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-31 03:02:07 +0900

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+@section('footer')
+<!-- SCRIPTS -->
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+@yield('addJs')
+</body>
+</html>
+@stop
