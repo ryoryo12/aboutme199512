bf05f11cf0a55d75f7e67b49704dd94089e4704c
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-30 16:07:21 +0900

    Create prof.blade.php

---
+++ resources/views/prof.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Profile</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+            <script>
+            </script>
+        </script>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Styles -->
+        <style>
+            
+            html, body {
+                background-color: #333333;
+                color: #777777;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .links a {
+                display: block;
+                margin: 150px; 
+                transform: rotate(270deg);
+                color: #777777;
+                right: 0;
+                font-size: 22px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            a{
+                color: #777777;
+                text-decoration: none;
+            }
+
+            .content {
+                height: 100vh;
+                width: 100%;
+                display: flex;
+            }
+
+            .left_content{
+                width: 14%;
+                height: 100%;
+                position: fixed;
+                border-right: solid 1px #FFA500;
+            }
+
+            .right_content{
+                width: 85%;
+                position: absolute;
+                right: 0px;
+                border-left: solid 1px #FF8C00;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <div class="left_content">
+                <ul class="links">
+                    <a href="/prof">Profile</a>
+                    <a href="/work">Works</a>
+                    <a href="https://github.com/ryoryo12">GitHub</a>
+                    <a href="/contact_form">Contact</a>
+                </ul>
+            </div>
+            <div class="right_content">
+            </div>
+        </div>
+    </body>
+</html>
