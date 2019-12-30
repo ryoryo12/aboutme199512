b4ecaa68f863b8c875023ad94e953465794734bf
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-12-31 04:49:31 +0900

    Update work.blade.php

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <!-- Styles -->
         <style>
             
