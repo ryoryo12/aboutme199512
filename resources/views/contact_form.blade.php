07562aed75e47f70808fbe1d21eedaf91010e61b
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2020-01-02 21:15:54 +0900

    Update contact_form.blade.php

--- resources/views/contact_form.blade.php
+++ resources/views/contact_form.blade.php
@@ -12,12 +12,38 @@ header('X-FRAME-OPTIONS: SAMEORIGIN');
 <html>
     <head>
 
+
         <meta charset="utf-8">
 
         <title>メールフォーム</title>
 
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+                <script type="text/javascript">
+                    timerID = setInterval('clock()',500); //0.5秒毎にclock()を実行
+
+                    function clock() {
+                        document.getElementById("view_clock").innerHTML = getNow();
+                    }
+
+                    function getNow() {
+                        var now = new Date();
+                        var year = now.getFullYear();
+                        var mon = now.getMonth()+1; //１を足すこと
+                        var day = now.getDate();
+                        var hour = now.getHours();
+                        var min = now.getMinutes();
+                        var sec = now.getSeconds();
+
+                        //出力用
+                        var s = year + "/" + mon + "/" + day + "/" + hour + ":" + min + ":" + sec; 
+                        return s;
+                    }
+                    
+                </script>
 
         <style>
 
@@ -74,6 +100,34 @@ header('X-FRAME-OPTIONS: SAMEORIGIN');
               text-transform: uppercase;
           }
 
+          .footer{
+                height: 8vh;
+                position: fixed;
+                width: 70%;
+                bottom: 0px;
+                left: 15%;
+            }
+            .footer_text_box{
+                text-align: center;
+                bottom: 0px;
+                margin-top: 4vh;
+            }
+
+            i{
+                display: inline;
+                font-size: 20px;
+            }
+
+            .footer_text{
+                display: inline;
+                font-size: 30px;
+                font-variant: all-petite-caps;
+            }
+
+            span{
+                font-size: 20px;
+            }
+
           
         </style>
       </head>
@@ -99,6 +153,14 @@ header('X-FRAME-OPTIONS: SAMEORIGIN');
                     <input type="hidden" name="token" value="<?=sha1(session_id())?>">
                     <input type="submit" value="確認する">
                 </form>
+            </div>
+
+            <div class="footer">
+                <div class="footer_text_box">
+                    <i class="fas fa-at"></i>
+                    <div class='footer_text'>ryoryo's portfolio</div>
+                    <span id="view_clock"></span>
+                </div>
             </div>
         </div>
 
