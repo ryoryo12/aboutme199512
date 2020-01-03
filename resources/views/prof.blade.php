8e75610513a5b281bdbc7ef2359324143ad30f55
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2020-01-03 17:35:48 +0900

    Update prof.blade.php

--- resources/views/prof.blade.php
+++ resources/views/prof.blade.php
@@ -176,15 +176,12 @@
                     <div class="content_text_name">
                     平田 亮太
                     </div>
-                    <div class="content_text">
-                    </div>
                     <div class="content_text_title">HISTORY</div>
                     <div class="content_text">
                     新卒で某大手住宅会社に営業として入社後、1年4ヶ月で退職。その後TECH：EXPERTにてRuby on Rails、Javascriptを用いたフルスクラッチのアプリケーション開発及びAWSを用いたデプロイ、Haml・SCSS・Ruby・Ruby on Rails・JavaScript・jQuery・GitHub・AWS　等の技術を用いた複合的機能の企画・実装を経験。
                     </div>
                     <div class="content_text_title">SKILLS</div>
                     <div class="content_text">
-                    
                     </div>
                 </div>
             </div>
