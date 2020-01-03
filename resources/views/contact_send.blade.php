b5496290fa46f643714467e6b0247e5fee194d4b
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2020-01-03 23:55:43 +0900

    Update contact_send.blade.php

--- resources/views/contact_send.blade.php
+++ resources/views/contact_send.blade.php
@@ -35,14 +35,12 @@ mb_internal_encoding('UTF-8');
 $header = 'From: ' . mb_encode_mimeheader($name). ' <' . $mail. '>';
 
 if (mb_send_mail($mailTo, $subject, $body, $header, '-f'. $returnMail)) {
-	
 	//セッション変数を全て解除
 	$_SESSION = array();
-	
         //セッションクッキーの削除
-	if (isset($_COOKIE["PHPSESSID"])) {
-		setcookie("PHPSESSID", '', time() - 1800, '/');
-	}
+		if (isset($_COOKIE["PHPSESSID"])) {
+				setcookie("PHPSESSID", '', time() - 1800, '/');
+			}
 
  	//セッションを破棄する
 		session_destroy();
